fa4fe306a5ee45f05f38f04b6c529410e000b91c
mlange-42 <martin_lange_@gmx.net>
2024-06-09 01:29:59 +0200

    render corner edges

--- internal/tui/render.go
+++ internal/tui/render.go
@@ -39,42 +39,88 @@ func (a *App) renderEdge(from, to int) {
 	vOverlap := b1.Y+b1.H >= b2.Y && b1.Y <= b2.Y+b2.H
 	if hOverlap {
 		if !vOverlap {
-			a.renderVertical(b1, b2)
+			a.renderEdgeVertical(b1, b2)
 		}
 	} else if vOverlap {
 		if !hOverlap {
-			a.renderHorizontal(b1, b2)
+			a.renderEdgeHorizontal(b1, b2)
 		}
+	} else {
+		a.renderEdgeCorner(b1, b2)
 	}
 }
 
-func (a *App) renderVertical(b1, b2 Bounds) {
+func (a *App) renderEdgeVertical(b1, b2 Bounds) {
 	xMid := (max(b1.X, b2.X) + min(b1.X+b1.W, b2.X+b2.W)) / 2
 	if b1.Y < b2.Y {
 		for y := b1.Y + b1.H; y < b2.Y; y++ {
-			a.canvas[y][xMid] = BorderH[0]
+			a.canvas[y][xMid] = BorderV[0]
 		}
-		a.canvas[b2.Y-1][xMid] = ArrowRight
+		a.canvas[b2.Y-1][xMid] = ArrowDown
 	} else {
 		for y := b2.Y + b2.H; y < b1.Y; y++ {
-			a.canvas[y][xMid] = BorderH[0]
+			a.canvas[y][xMid] = BorderV[0]
 		}
-		a.canvas[b1.Y-1][xMid] = ArrowLeft
+		a.canvas[b2.Y+b2.H][xMid] = ArrowUp
 	}
 }
 
-func (a *App) renderHorizontal(b1, b2 Bounds) {
+func (a *App) renderEdgeHorizontal(b1, b2 Bounds) {
 	yMid := (max(b1.Y, b2.Y) + min(b1.Y+b1.H, b2.Y+b2.H)) / 2
 	if b1.X < b2.X {
 		for x := b1.X + b1.W; x < b2.X; x++ {
-			a.canvas[yMid][x] = BorderV[0]
+			a.canvas[yMid][x] = BorderH[0]
 		}
-		a.canvas[yMid][b2.Y-1] = ArrowDown
+		a.canvas[yMid][b2.X-1] = ArrowRight
 	} else {
 		for x := b2.X + b2.W; x < b1.X; x++ {
-			a.canvas[yMid][x] = BorderV[0]
+			a.canvas[yMid][x] = BorderH[0]
 		}
-		a.canvas[yMid][b1.X-1] = ArrowUp
+		a.canvas[yMid][b1.X-1] = ArrowLeft
+	}
+}
+
+func (a *App) renderEdgeCorner(b1, b2 Bounds) {
+	downwards := b2.Y > b1.Y
+	rightwards := b2.X > b1.X
+
+	yStart := b1.Y
+	if downwards {
+		yStart = b1.Y + b1.H - 1
+	}
+	var xStart int
+	if rightwards {
+		for x := b1.X + b1.W + 1; x < b2.X; x++ {
+			a.canvas[yStart][x] = BorderH[0]
+		}
+		xStart = b2.X
+		if downwards {
+			a.canvas[yStart][xStart] = BorderNE[0]
+		} else {
+			a.canvas[yStart][xStart] = BorderSE[0]
+		}
+	} else {
+		for x := b1.X - 1; x >= b2.X+b2.W; x-- {
+			a.canvas[yStart][x] = BorderH[0]
+		}
+		xStart = b2.X + b2.W - 1
+		if downwards {
+			a.canvas[yStart][xStart] = BorderNW[0]
+		} else {
+			a.canvas[yStart][xStart] = BorderSW[0]
+		}
+	}
+
+	if downwards {
+		for y := yStart + 1; y < b2.Y; y++ {
+			a.canvas[y][xStart] = BorderV[0]
+		}
+		a.canvas[b2.Y-1][xStart] = ArrowDown
+	} else {
+		for y := yStart - 1; y > b2.Y+b2.H; y-- {
+			a.canvas[y][xStart] = BorderV[0]
+		}
+		a.canvas[b2.Y+b2.H][xStart] = ArrowUp
 	}
 }
 
