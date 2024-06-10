2790da2e8a4985a67f4e59424bbc9f074883cefd
mlange-42 <martin_lange_@gmx.net>
2024-06-10 21:45:49 +0200

    parse node positions, update README

--- util.go
+++ util.go
@@ -155,6 +155,29 @@ func FromBIFXML(content []byte) (*Network, []*Node, error) {
 			table[i] = row
 		}
 		position := [2]int{}
+		for _, prob := range variable.Properties {
+			parts := strings.Split(prob, "=")
+			if len(parts) != 2 || strings.TrimSpace(parts[0]) != "position" {
+				continue
+			}
+			value := strings.TrimSpace(parts[1])
+			value = strings.Trim(value, "()")
+			parts = strings.Split(value, ",")
+			if len(parts) != 2 {
+				return nil, nil, fmt.Errorf("syntax error in property 'position' of node '%s'", variable.Name)
+			}
+			var err error
+			position[0], err = strconv.Atoi(strings.TrimSpace(parts[0]))
+			if err != nil {
+				return nil, nil, fmt.Errorf("error parsing '%s' to integer in property 'position' of node '%s'", parts[0], variable.Name)
+			}
+			position[1], err = strconv.Atoi(strings.TrimSpace(parts[1]))
+			if err != nil {
+				return nil, nil, fmt.Errorf("error parsing '%s' to integer in property 'position' of node '%s'", parts[1], variable.Name)
+			}
+			position[0] /= 2
+			position[1] /= 12
+		}
 
 		node := Node{
 			Variable: variable.Name,
