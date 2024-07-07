71bb8764bcab8a84d7f21a8d8f4abf71ccab656e
mlange-42 <martin_lange_@gmx.net>
2024-07-07 21:10:11 +0200

    document package `logic`

---
+++ logic/doc.go
@@ -0,0 +1,5 @@
+// Package logic provides shortcuts to create factors/CPTs for logic operations.
+//
+// To get logic factors, use either [Get] to get them by name,
+// or one of the constructors of [Factor].
+package logic
